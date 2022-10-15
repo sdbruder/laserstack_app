6e94af16499dfffad7741243df98f0627be092ed
Sergio Bruder <sergio@bruder.com.br>
2022-10-15 05:05:50 +0200

    build.sh

--- build.sh
+++ build.sh
@@ -9,9 +9,16 @@ fi
 
 base_dir="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
+docker login --username "$HUB_USER" --password "$HUB_PASSWORD"
 for version in $(ls -r -d */ | cut -f1 -d'/' | xargs) ; do
+    if [ "$APP_LATEST" == "$version" ]; then
+        EXTRA="--tag laserstack/app:latest"
+    else
+        EXTRA=""
+    fi
     cd $base_dir/$version
     echo "$version ----------------------------"
-    docker build -t laserstack/app:$version .
-    docker push     laserstack/app:$version
+    docker build --tag laserstack/app:$version $EXTRA .
 done
+docker push --all-tags laserstack/app
+
