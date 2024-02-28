3b0695a6c4f9fc21c0e6728a3deba0a952d8935a
Sergio Bruder <sergio@bruder.com.br>
2024-02-28 02:24:34 +0100

    dynamic minor version tagging

--- buildx.sh
+++ buildx.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 # load .env
 if [ -f .env ]; then
-    export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
+	export $(echo $(cat .env | sed 's/#.*//g' | xargs) | envsubst)
 fi
 
 base_dir="$(dirname "$(readlink -f "$BASH_SOURCE")")"
@@ -12,18 +12,25 @@ base_dir="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 docker login --username "$HUB_USER" --password "$HUB_PASSWORD"
 docker buildx create --name laserbuilder --use --bootstrap
 
-for version in $(ls -r -d */ | cut -f1 -d'/' | xargs) ; do
-    if [ "$APP_LATEST" == "$version" ]; then
-        EXTRA="--tag laserstack/app:latest"
-    else
-        EXTRA=""
-    fi
-    cd $base_dir/$version
-    echo "$version ----------------------------"
-    docker buildx build 			\
-    	--push					\
-	--platform linux/amd64,linux/arm64 	\
-	--tag laserstack/app:$version $EXTRA 	\
-	.
+for version in $(ls -r -d */ | cut -f1 -d'/' | xargs); do
+	if [ "$APP_LATEST" == "$version" ]; then
+		EXTRA="--tag laserstack/app:latest"
+	else
+		EXTRA=""
+	fi
+	cd $base_dir/$version
+	echo "$version ----------------------------"
+
+	ALPINE=$(grep FROM Dockerfile | awk '{ print $2;}')
+	PHP=$(grep PHP Dockerfile | awk '{ print $2;}')
+	PHPVERSION=$(docker run -it --rm $ALPINE /bin/ash -c "apk add $PHP ; $PHP --version" | grep ^PHP | awk '{print $2}')
+	EXTRA="--tag laserstack/app:$PHPVERSION $EXTRA"
+
+	docker \
+		buildx build \
+		--push \
+		--platform linux/amd64,linux/arm64 \
+		--tag laserstack/app:$version $EXTRA \
+		.
 done
 docker buildx rm laserbuilder
